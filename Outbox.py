7c61acf8bbdbe49f597022083c189b9fd2539615
Grant Stafford <grant.d.stafford@gmail.com>
2012-11-03 03:36:43 -0400

    Outbox for sending text messgaes and minor changes to Inbox

---
+++ Outbox.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+class Outbox:
+    """ Handles sending messages out from a game """
+    
+    def __init__(self, outputTypes=["gmail"],
+                gmailAddress="texttokill@gmail.com",
+                gmailPassword="murdermystery"):
+        self.outputTypes = outputTypes
+        self.gmailAddress = gmailAddress
+        self.gmailPassword = gmailPassword
+
+    def sendText(self, to, text, subject="MOD"):
+        """ sends a text with subject and text to a player """
+        #msg = MIMEMultipart()
+        msg = MIMEText(text)
+
+        msg['From'] = self.gmailAddress
+        msg['To'] = to
+        msg['Subject'] = subject
+
+        #msg.attach(MIMEText(text))
+
+        mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(self.gmailAddress, self.gmailPassword)
+        mailServer.sendmail(self.gmailAddress, to, msg.as_string())
+        mailServer.close()
