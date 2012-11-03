65225685dcc225fe4230bfd541600284bf618e7c
Grant Stafford <grant.d.stafford@gmail.com>
2012-11-03 08:32:31 -0400

    Tests the game loop

---
+++ Test.py
@@ -0,0 +1,11 @@
+from Game import Game
+from Player import Player
+from Character import Character
+from Ability import Ability
+from Abilities.KillAbility import KillAbility
+from Abilities.TruthtellAbility import TruthtellAbility
+from Abilities.AbilitiesAbility import AbilitiesAbility
+
+def test():
+    game = Game([AbilitiesAbility(), TruthtellAbility()], [None, Player("Samira", "5036168317@vtext.com", Character("Char",[], ["Truthtell"], "desc")) ])
+    game.run()
