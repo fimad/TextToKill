2edcbe0555e8690ecdb05e9b772ced308593c3f4
Michael Vassilevsky <mvassilevsky@gmail.com>
2012-11-03 00:11:59 -0700

    abilities

---
+++ abilities.py
@@ -0,0 +1,45 @@
+#do not send if blank string
+
+class Ability:
+    abilityName = ''
+    keyword = ''
+    toTarget = ''
+
+class Truthtell(Ability):
+    abilityName = 'Truthtell'
+    keyword = 'truthtell'
+    toTarget = 'A truthell is being used on you. Find the game master.'
+    #remove from owner's list of abilities
+    
+class Steal(Ability):
+    abilityName = 'Steal'
+    keyword = 'steal'
+    abilityToSteal = '' #take input
+    toTarget = 'Your ' + abilityToSteal + ' is being stolen.'
+    #remove from owner's list of abilities
+
+class Kill(Ability):
+    abilityName = 'Kill'
+    keyword = 'kill'
+    toTarget = 'You are dying. You will die in 15 minutes unless a save is used on you.'
+    toAll = playerName + ' is being killed and has 15 minutes to live if not saved.' #fix
+    #put kill on queue
+    #remove from owner's list of abilities
+
+class Save(Ability):
+    abilityName = 'Save'
+    keyword = 'save'
+    toTarget = ''
+    toAll = ''
+    if playerIsDying: #fix
+        if playerTimesDying == 1: #fix
+            toAll = playerName + ' is no longer dying.' #fix
+        else:
+            toAll = playerName + ' has had one save used on them. They are now dying ' + str(playerTimesDying) + ' times.' #fix
+        #remove oldest active kill on queue
+        #remove from owner's list of abilities
+    else:
+        #send playerName + ' is not dying' to user
+        
+class Error(Ability):
+    
