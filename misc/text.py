c0de930436929242a4fdc009891688d8bba6390a
Gary Lent <gjl12009@mymail.pomona.edu>
2012-11-02 22:35:47 -0700

    Adds script for sending texts through gmail

---
+++ misc/text.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "texttokill@gmail.com"
+gmail_pwd = "murdermystery"
+
+def text(to, subject, text):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   mailServer.close()
